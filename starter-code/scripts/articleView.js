3d57879ceb5ed3278283820c5d721d23a4578f17
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2017-04-17 11:45:42 -0700

    drying out code more

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -71,14 +71,10 @@ articleView.setTeasers = function () {
 articleView.initNewArticlePage = function () {
   $('.tab-content').show();
 
-  // TODO: The new articles we create will be copy/pasted into our source data file.
-  // Set up this "export" functionality. We can hide it for now, and show it once we have data to export.
   $('#article-json').on('focus', function () {
     this.select();
   });
 
-  // TODO: Add an event handler to update the preview and the export field with info
-  // from the article that WOULD BE created if any inputs change.
   $('#article-info').on('change', this.create);
 
 };
